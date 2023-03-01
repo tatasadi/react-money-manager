05e79949955fa2c72400fcbca7b62e9786b2f3f6
Ehsan Tatasadi <ehsan@tatasadi.com>
2023-03-01 18:21:23 +0100

    add bar chart

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -8,12 +8,44 @@ import {
   previousMonth,
 } from "../../utils";
 
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Pie } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+} from "chart.js";
+import { Pie, Line } from "react-chartjs-2";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title
+);
 
-export const chartDate = {
+const lineChartOptions = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Last 6 Month",
+    },
+  },
+};
+
+const pieChartDate = {
   labels: ["Income", "Expense"],
   datasets: [
     {
@@ -68,73 +100,114 @@ function BalanceStats() {
       name: "Total",
       income: totalIncome,
       expense: tatalExpense,
+      balance: totalIncome - tatalExpense,
     },
     {
       name: "Last Month",
       income: lastMonthIncome,
       expense: lastMonthExpense,
+      balance: lastMonthIncome - lastMonthExpense,
     },
     {
       name: "This Month",
       income: thisMonthIncome,
       expense: thisMonthExpense,
+      balance: thisMonthIncome - thisMonthExpense,
     },
   ];
 
+  const lineChartData = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "Income",
+        data: [lastMonthIncome, thisMonthIncome, 1000, 2000, 2500, 1500, 1700],
+        borderColor: "#86efac",
+        backgroundColor: "#16a34a",
+      },
+      {
+        label: "Expense",
+        data: [lastMonthExpense, thisMonthExpense, 600, 1300, 1700, 2000, 1700],
+        borderColor: "#fca5a5",
+        backgroundColor: "#dc2626",
+      },
+      {
+        label: "Balance",
+        data: [
+          lastMonthIncome - lastMonthExpense,
+          thisMonthIncome - thisMonthExpense,
+          400,
+          700,
+          800,
+          -500,
+          0,
+        ],
+        borderColor: "#a5b4fc",
+        backgroundColor: "#4f46e5",
+      },
+    ],
+  };
+
   return (
-    <dl className="mt-2 grid grid-cols-1 divide-y divide-gray-200 lg:grid-cols-3 md:divide-y-0 md:divide-x gap-4">
-      {stats.map((item) => {
-        const balance = item.income - item.expense;
-        return (
-          <div
-            key={item.name}
-            className="px-4 py-5 sm:p-6 bg-white shadow rounded-lg"
-          >
-            <dt className="text-base font-normal text-gray-900">{item.name}</dt>
-            <dd className="mt-1">
-              <div className="flex justify-between items-baseline font-semibold ">
-                <div className="text-gray-900">Income</div>
-                <div className="text-green-600 text-xl">
-                  {formatCurrency(item.income)}
+    <>
+      <dl className="mt-2 grid grid-cols-1 divide-y divide-gray-200 lg:grid-cols-3 md:divide-y-0 md:divide-x gap-4">
+        {stats.map((item) => {
+          return (
+            <div
+              key={item.name}
+              className="px-4 py-5 sm:p-6 bg-white shadow rounded-lg"
+            >
+              <dt className="text-base font-normal text-gray-900">
+                {item.name}
+              </dt>
+              <dd className="mt-1">
+                <div className="flex justify-between items-baseline font-semibold ">
+                  <div className="text-gray-900">Income</div>
+                  <div className="text-green-600 text-xl">
+                    {formatCurrency(item.income)}
+                  </div>
                 </div>
-              </div>
-              <div className="flex justify-between font-semibold">
-                <div className="text-gray-900">Expense</div>
-                <div className="text-red-600 text-xl">
-                  {formatCurrency(item.expense)}
+                <div className="flex justify-between font-semibold">
+                  <div className="text-gray-900">Expense</div>
+                  <div className="text-red-600 text-xl">
+                    {formatCurrency(item.expense)}
+                  </div>
                 </div>
-              </div>
-              <div className="flex justify-between font-semibold border-t-2 mt-4">
-                <div className="text-gray-900">Balance</div>
-                <div
-                  className={classNames(
-                    balance > 0 ? "text-green-600" : "text-red-600",
-                    "text-xl"
-                  )}
-                >
-                  {formatCurrency(balance)}
+                <div className="flex justify-between font-semibold border-t-2 mt-4">
+                  <div className="text-gray-900">Balance</div>
+                  <div
+                    className={classNames(
+                      item.balance > 0 ? "text-green-600" : "text-red-600",
+                      "text-xl"
+                    )}
+                  >
+                    {formatCurrency(item.balance)}
+                  </div>
                 </div>
-              </div>
-              {balance !== 0 && (
-                <div className="flex justify-center items-center xl:p-10 lg:p-5 p-10">
-                  <Pie
-                    data={{
-                      ...chartDate,
-                      datasets: [
-                        {
-                          ...chartDate.datasets[0],
-                          data: [item.income, item.expense],
-                        },
-                      ],
-                    }}
-                  />
-                </div>
-              )}
-            </dd>
-          </div>
-        );
-      })}
-    </dl>
+                {item.balance !== 0 && (
+                  <div className="flex justify-center items-center xl:p-10 lg:p-5 p-10">
+                    <Pie
+                      data={{
+                        ...pieChartDate,
+                        datasets: [
+                          {
+                            ...pieChartDate.datasets[0],
+                            data: [item.income, item.expense],
+                          },
+                        ],
+                      }}
+                    />
+                  </div>
+                )}
+              </dd>
+            </div>
+          );
+        })}
+      </dl>
+      <div className="bg-white rounded-lg shadow mt-4 p-10">
+        <Line options={lineChartOptions} data={lineChartData} />
+      </div>
+    </>
   );
 }
 
